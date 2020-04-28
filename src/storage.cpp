aadd3124f3579ee276167ab30de4c77b7341f9b4
RealTonyShaw <tonyshaw123@hotmail.com>
2020-04-28 22:01:16 +0800

    Update format of storage.cpp

--- src/storage.cpp
+++ src/storage.cpp
@@ -10,20 +10,20 @@ using namespace std;
 
 namespace badgerdb {
 
-RecordId HeapFileManager::insertTuple(const string& tuple,
-                                      File& file,
-                                      BufMgr* bufMgr) {
-  // TODO
-}
+    RecordId HeapFileManager::insertTuple(const string &tuple,
+                                          File &file,
+                                          BufMgr *bufMgr) {
+        // TODO
+    }
 
-void HeapFileManager::deleteTuple(const RecordId& rid,
-                                  File& file,
-                                  BufMgr* bugMgr) {
-  // TODO
-}
+    void HeapFileManager::deleteTuple(const RecordId &rid,
+                                      File &file,
+                                      BufMgr *bugMgr) {
+        // TODO
+    }
 
-string HeapFileManager::createTupleFromSQLStatement(const string& sql,
-                                                    const Catalog* catalog) {
-  // TODO
-}
+    string HeapFileManager::createTupleFromSQLStatement(const string &sql,
+                                                        const Catalog *catalog) {
+        // TODO
+    }
 }  // namespace badgerdb
